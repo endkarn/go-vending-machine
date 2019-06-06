5fb39275bfdabb5ad4c488aeee39e9ef14618ed8
Karnawat <endkarn0@gmail.com>
2019-06-06 12:37:09 +0700

    ปรับปรุง : เปลี่ยนการ arrange ค่าเหรียญมาเป็นการกำหนดค่าใน action

--- insert_coin_test.go
+++ insert_coin_test.go
@@ -6,11 +6,10 @@ func TestInsertCoinBy_T_VendingShouldHaveBalance_10(t *testing.T) {
 
 	//arrange
 	v := VendingMachine{}
-	coin := "T"
 	expectedResult := 10
 
 	//action
-	actualResult := v.insertCoin(coin)
+	actualResult := v.insertCoin("T")
 
 	//assert
 	if actualResult != expectedResult {
@@ -23,11 +22,10 @@ func TestInsertCoinBy_F_VendingShouldHaveBalance_F(t *testing.T) {
 
 	//arrange
 	v := VendingMachine{}
-	coin := "F"
 	expectedResult := 5
 
 	//action
-	actualResult := v.insertCoin(coin)
+	actualResult := v.insertCoin("F")
 
 	//assert
 	if actualResult != expectedResult {
@@ -40,11 +38,10 @@ func TestInsertCoinBy_TW_VendingShouldHaveBalance_2(t *testing.T) {
 
 	//arrange
 	v := VendingMachine{}
-	coin := "TW"
 	expectedResult := 2
 
 	//action
-	actualResult := v.insertCoin(coin)
+	actualResult := v.insertCoin("TW")
 
 	//assert
 	if actualResult != expectedResult {
@@ -57,11 +54,10 @@ func TestInsertCoinBy_O_VendingShouldHaveBalance_1(t *testing.T) {
 
 	//arrange
 	v := VendingMachine{}
-	coin := "O"
 	expectedResult := 1
 
 	//action
-	actualResult := v.insertCoin(coin)
+	actualResult := v.insertCoin("O")
 
 	//assert
 	if actualResult != expectedResult {
@@ -75,11 +71,10 @@ func TestInsertCoinBy_ABC_VendingShouldHaveBalance_0(t *testing.T) {
 
 	//arrange
 	v := VendingMachine{}
-	coin := "ABC"
 	expectedResult := 0
 
 	//action
-	actualResult := v.insertCoin(coin)
+	actualResult := v.insertCoin("ABC")
 
 	//assert
 	if actualResult != expectedResult {
