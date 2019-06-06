6ade4f76e5e7a181fcec4f9da5f56adce4f32f9c
Karnawat <endkarn0@gmail.com>
2019-06-06 12:23:15 +0700

    Refactor : ลดการเงื่อนไข if

--- vendingmachine.go
+++ vendingmachine.go
@@ -6,18 +6,5 @@ func insertCoin(coin string) int {
 	coinMap["F"] = 5
 	coinMap["TW"] = 2
 	coinMap["O"] = 1
-
-	if coin == "T" {
-		return coinMap["T"]
-	}
-	if coin == "F" {
-		return coinMap["F"]
-	}
-	if coin == "TW" {
-		return coinMap["TW"]
-	}
-	if coin == "O" {
-		return coinMap["O"]
-	}
-	return 0
+	return coinMap[coin]
 }
